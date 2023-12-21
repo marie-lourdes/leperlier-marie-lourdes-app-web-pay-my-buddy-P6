4bf033667d517b2109a2d10a9eb0bebd7d2346be
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-12-21 17:59:05 +0100

    data.sql: refactor auto imcrement id account and id transaction since value 76000 for account and since 0000 for transaction

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,10 @@
 CREATE TABLE user_app(
 email VARCHAR(500) NOT NULL UNIQUE PRIMARY KEY,
-name VARCHAR(100) NOT NULL,
+first_name VARCHAR(100) NOT NULL,
 last_name VARCHAR(100) NOT NULL,
 password VARCHAR(100) NOT NULL,
-user_contact_id VARCHAR(500) UNIQUE 
-FOREIGN KEY(user_contact_id ) REFERENCES user_app(email) ON DELETE SET NULL ON UPDATE CASCADE,
-(email)
+user_contact_id VARCHAR(500) UNIQUE, 
+FOREIGN KEY(user_contact_id ) REFERENCES user_app(email) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 CREATE TABLE role(
@@ -25,7 +24,7 @@ PRIMARY KEY(user_id,role_id)
 );
 
 CREATE TABLE account(
-id BIGINT NOT NULL AUTO_INCREMENT DEFAULT 760000,
+id BIGINT NOT NULL AUTO_INCREMENT,
 user_id VARCHAR(500) NOT NULL,
 soldes DECIMAL NOT NULL,
 TYPE VARCHAR(100),
@@ -33,8 +32,10 @@ FOREIGN KEY(user_id) REFERENCES user_app(email) ON DELETE CASCADE ON UPDATE CASC
 PRIMARY KEY(id,user_id )
 );
 
+ALTER TABLE account AUTO_INCREMENT = 76000;
+
 CREATE TABLE transaction(
-id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY DEFAULT 00000,
+id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
 user_account_id  BIGINT NOT NULL,
 credit_user_id VARCHAR(500) NOT NULL,
 beneficiary_count_id BIGINT NOT NULL,
@@ -48,6 +49,7 @@ FOREIGN KEY(credit_user_id) REFERENCES account(user_id) ON DELETE RESTRICT ON UP
 FOREIGN KEY(beneficiary_count_id) REFERENCES account(id) ON DELETE RESTRICT ON UPDATE CASCADE,
 FOREIGN KEY(beneficiary_user_id) REFERENCES account(user_id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
+ALTER TABLE transaction AUTO_INCREMENT = 0000;
 
 /* DEFAULT VALUES FOR PAYMYBUDDY_TEST DB */
 
@@ -61,3 +63,9 @@ INSERT INTO user_app_role(user_id,role_id) VALUES
 ("testuser1@gmail.com",1),
 ("testuser1@gmail.com",2),
 ("testuser2@gmail.com",1);
+
+INSERT INTO account(id,user_id  ,soldes,TYPE ) VALUES
+("testuser1@gmail.com", 45075.00, "buddy account");
+
+INSERT INTO account(user_id  ,soldes,TYPE ) VALUES
+("testuser2@gmail.com", 9825.00, "buddy account");
