fc85d8b603133e22d215f9b5d45cb756ad117784
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-12-22 14:01:25 +0100

    create securityFilterChain bean in class configuration to desactivate page login default of spring security

--- src/main/java/com/paymybuddy/api/config/security/WebSecurity.java
+++ src/main/java/com/paymybuddy/api/config/security/WebSecurity.java
@@ -1,8 +1,30 @@
 package com.paymybuddy.api.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurity {
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests((requests) -> requests
+				.requestMatchers("/*").permitAll()
+				.anyRequest().authenticated()
+			)
+			.formLogin((form) -> form
+					.loginPage("/home")
+					.permitAll()
+				)
+				.logout((logout) -> logout.permitAll());
+		
+			
+
+		return http.build();
+	}
 
 }
