dce612f783c60abc3c0210c164f841a4274858c1
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-01-02 13:49:46 +0100

    IUserAppDTORepository.java: add method save()

--- src/main/java/com/paymybuddy/api/repository/IUserAppDTORepository.java
+++ src/main/java/com/paymybuddy/api/repository/IUserAppDTORepository.java
@@ -9,13 +9,17 @@ import com.paymybuddy.api.domain.model.UserApp;
 public interface IUserAppDTORepository extends JpaRepository<UserApp, String> {
 	public UserApp findByEmail(String Email);
 	
-	@Query("\r\n"
+	public UserApp save(String Email);
+	
+	
+	
+	/*@Query("\r\n"
 			+ "      SELECT role.role_name \r\n"
 			+ "        FROM user_app\r\n"
 			+ "        JOIN user_app_role ON(user_app.email=user_app_role.user_id )\r\n"
 			+ "        JOIN role ON(role.id=user_app_role.role_id )\r\n"
 			+ "        WHERE user_app.id =:email  ")
-	public String findByRole( @Param("email") String email);
+	public String findByRole( @Param("email") String email);*/
 
 
 }
