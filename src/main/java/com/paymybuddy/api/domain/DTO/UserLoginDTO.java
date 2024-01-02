090a7293bea13544471b75608a13d041756f428a
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-01-02 20:14:48 +0100

    implementation-authentication-user-functionality: create UserMapper

--- src/main/java/com/paymybuddy/api/domain/DTO/UserLoginDTO.java
+++ src/main/java/com/paymybuddy/api/domain/DTO/UserLoginDTO.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.paymybuddy.api.domain.model.Role;
 
+import lombok.Data;
+
 @Data
 public class UserLoginDTO {
 	private String email;
--- src/main/java/com/paymybuddy/api/domain/DTO/UserMapper.java
+++ src/main/java/com/paymybuddy/api/domain/DTO/UserMapper.java
@@ -1,5 +1,12 @@
 package com.paymybuddy.api.domain.DTO;
 
-public class UserMapper {
+import org.springframework.stereotype.Component;
 
+import com.paymybuddy.api.domain.model.UserApp;
+
+@Component
+public class UserMapper {
+	public UserApp DTOtoUser(UserLoginDTO userDTO) {
+        return new UserApp(userDTO.getEmail(), userDTO.getPassword(), userDTO.getRoles());
+    }
 }
