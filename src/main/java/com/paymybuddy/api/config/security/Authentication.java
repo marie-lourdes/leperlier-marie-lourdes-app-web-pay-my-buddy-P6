2d832b4e17f32d1ac285f6582849d17a05469f57
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-01-02 16:25:14 +0100

    test-authentication-user: create class configuration with Bean  AuthenticationManager and BCryptPasswordEncoder

--- src/main/java/com/paymybuddy/api/config/security/Authentication.java
+++ src/main/java/com/paymybuddy/api/config/security/Authentication.java
@@ -1,21 +1,36 @@
 package com.paymybuddy.api.config.security;
 
-/*@Autowired
-private AuthenticationUserDetailService authentificationService;*/
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-/*@Configuration
+import com.paymybuddy.api.service.AuthenticationUserDetailService;
+
+
+
+@Configuration
 @EnableWebSecurity
 public class Authentication {
-	/*@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();g
-	}*/
+	@Autowired
+	private AuthenticationUserDetailService authentificationService;
 	
-	/*@Bean
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
 	public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder) throws Exception {
 		AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-	authenticationManagerBuilder.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder);
+	authenticationManagerBuilder.userDetailsService(authentificationService).passwordEncoder(bCryptPasswordEncoder);
 		return authenticationManagerBuilder.build();
 	}
+}
 
-}*/
+
+
