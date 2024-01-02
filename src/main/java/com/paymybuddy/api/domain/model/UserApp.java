83aab7f117a24ee365aa2d74bf99d59d2b54d973
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-01-02 15:20:11 +0100

    test-authentication-user: add attributes roles and contacts to UserApp Entity

--- src/main/java/com/paymybuddy/api/domain/model/UserApp.java
+++ src/main/java/com/paymybuddy/api/domain/model/UserApp.java
@@ -2,11 +2,17 @@ package com.paymybuddy.api.domain.model;
 
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
@@ -33,7 +39,19 @@ public  class UserApp {
 	@Column(name="password")
 	private String password;
 	
-	@Column(name="contacts")
+	@OneToMany	( cascade = CascadeType.ALL, 
+			   orphanRemoval = true, 
+			   fetch = FetchType.EAGER)
+	@JoinColumn(name="user_contact_id")
 	private List<UserApp> contacts;
+	
+	@ManyToMany
+	@JoinTable( 
+	        name = "user_app_role", 
+	        joinColumns = @JoinColumn(
+	          name = "user_id", referencedColumnName = "id"), 
+	        inverseJoinColumns = @JoinColumn(
+	          name = "role_id", referencedColumnName = "id")) 
+	private List<Role> roles;
 
 }
