ee668207b4550cc75d5a36f76833298078013e6d
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-01-02 15:23:12 +0100

    test-authentication-user: add attributes users with annotation @ManyToMany

--- src/main/java/com/paymybuddy/api/domain/model/Role.java
+++ src/main/java/com/paymybuddy/api/domain/model/Role.java
@@ -1,10 +1,13 @@
 package com.paymybuddy.api.domain.model;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -18,5 +21,8 @@ public class Role {
 	
 	@Column(name="role_name")
 	private String roleName;
+	
+	@ManyToMany(mappedBy="roles")	
+	private List<UserApp>users;
 
 }
