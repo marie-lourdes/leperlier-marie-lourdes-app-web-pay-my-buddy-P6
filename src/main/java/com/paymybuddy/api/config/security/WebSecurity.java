34d307f2550c0e8c705c0a84d690f2b5dbb8ac31
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-01-03 11:28:09 +0100

    WebSecurity.java: add rememberme with configuration remembermeservices and add deleteCookies() JSESSIONID generated

--- src/main/java/com/paymybuddy/api/config/security/WebSecurity.java
+++ src/main/java/com/paymybuddy/api/config/security/WebSecurity.java
@@ -1,27 +1,54 @@
 package com.paymybuddy.api.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices.RememberMeTokenAlgorithm;
+
+import com.paymybuddy.api.service.AuthenticationUserDetailService;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurity  {
+public class WebSecurity {
+	
+	@Autowired
+	private AuthenticationUserDetailService authentificationService;
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		// ajoutez les roles
 		http.authorizeHttpRequests((requests) -> {
 			requests.requestMatchers("/sign-up").permitAll();
 			requests.requestMatchers("/transactions").hasRole("ADMIN");
-			requests.requestMatchers("/login").rememberMe();
-			requests.anyRequest().authenticated();
-			
-		}).formLogin((form) -> form.loginPage("/login").permitAll().defaultSuccessUrl("/account/home"))
-				.logout((logout) -> logout.permitAll());
-				
-		
+			// requests.requestMatchers("/login").rememberMe();
+			requests.anyRequest().authenticated().anyRequest();
+
+		}).rememberMe((remember) -> {
+			remember.rememberMeServices(rememberMeServices(authentificationService));
+			remember.useSecureCookie(true);
+		})
+		.formLogin((form) -> form.loginPage("/login").permitAll().defaultSuccessUrl("/account/home"))
+		.logout((logout) -> {
+			logout.permitAll();
+			logout.deleteCookies("JSESSIONID");
+		});
+
 		return http.build();
+
+	}
+
+	@Bean
+	RememberMeServices rememberMeServices(UserDetailsService userDetailsService) {
+		RememberMeTokenAlgorithm encodingAlgorithm = RememberMeTokenAlgorithm.SHA256;
+		TokenBasedRememberMeServices rememberMe = new TokenBasedRememberMeServices("tokensecret", userDetailsService,
+				encodingAlgorithm);
+		rememberMe.setMatchingAlgorithm(RememberMeTokenAlgorithm.MD5);
+		return rememberMe;
 	}
 }
