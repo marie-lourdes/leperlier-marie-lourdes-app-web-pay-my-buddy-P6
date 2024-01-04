cc63861dc6db49f7d46a74891f8ee482e61b3624
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-01-04 17:48:55 +0100

    delete page error-403, error-500

--- src/main/java/com/paymybuddy/api/ApiApplication.java
+++ src/main/java/com/paymybuddy/api/ApiApplication.java
@@ -2,7 +2,9 @@ package com.paymybuddy.api;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 //@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 @SpringBootApplication
--- src/main/java/com/paymybuddy/api/controller/LoginController.java
+++ src/main/java/com/paymybuddy/api/controller/LoginController.java
@@ -1,33 +1,62 @@
 package com.paymybuddy.api.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 public class LoginController {
-	
+
 	@GetMapping("/sign-up")
 	public String getSignUpPage() {
 		return "sign-up";
 	}
-	
-	@GetMapping("/login")//
-	public String  getLoginPage() {
+
+	@GetMapping("/login") //
+	public String getLoginPage() {
 		return "login";
 	}
-	
-	/*@PostMapping("/login")//traitement formulaire et generation token avec JWT
-	public ModelAndView loginForm() {
-		return new ModelAndView("redirect:/");	
-	}*/
-	
+
+	/*
+	 * @PostMapping("/login")//traitement formulaire et generation token avec JWT
+	 * public ModelAndView loginForm() { return new ModelAndView("redirect:/"); }
+	 */
+
 	@GetMapping("/account/home")
-	public String  getHomePage() {
+	public String getHomePage() {
 		return "home";
 	}
+
+
+	@GetMapping("/transactions")
+	public String getTransactionPage() {
+		return"home";
+	}
 	
+	/*@GetMapping("/error")
+	public String getPage404() {
+		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+		if (status != null) {
+			Integer statusCode = Integer.valueOf(status.toString());
+
+			if (statusCode == HttpStatus.NOT_FOUND.value()) {
+				return "error-404";
+			} else if (statusCode == HttpStatus.FORBIDDEN.value()) {
+				return "error-403";
+			}
+
+		}
+
+		return "error";
+	}*/
+
 	@GetMapping("/error")
-	public String  getPage404() {	   
+	public String getPage404() {
 		return "error";
 	}
+	
 }
