d71a42d634e3a3291d36677e26f1361b777269bf
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-01-07 15:57:08 +0100

    MessageTestingAuthenticationService.java: implements method getMessage with preauthorize to test authentication in class under test AuthenticationUserDetailServiceTest

--- src/main/java/com/paymybuddy/api/utils/MessageTestingAuthenticationService.java
+++ src/main/java/com/paymybuddy/api/utils/MessageTestingAuthenticationService.java
@@ -1,5 +1,15 @@
 package com.paymybuddy.api.utils;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.paymybuddy.api.config.security.Authentication;
+
 public class MessageTestingAuthenticationService  implements MessageService{
+	@PreAuthorize("authenticated")//check the expression authenticated with the method getMessage()
+    public String getMessage() {
+       Authentication authentication = (Authentication) SecurityContextHolder.getContext().getAuthentication();
+      return  "Hello" + authentication ;
+   }
 
 }
