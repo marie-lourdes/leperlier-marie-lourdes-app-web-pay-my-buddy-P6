be1f524fb605d45e2e6343b525b4f14e67ac17a3
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-01-08 13:51:29 +0100

    UserRepositpory.java: change return value of abstract method findByEmail()

--- src/main/java/com/paymybuddy/api/repository/IUserRepository.java
+++ src/main/java/com/paymybuddy/api/repository/IUserRepository.java
@@ -1,5 +1,7 @@
 package com.paymybuddy.api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.paymybuddy.api.domain.model.UserApp;
@@ -8,7 +10,7 @@ public interface IUserRepository extends JpaRepository<UserApp, String> {
 	
 	/*@Query("SELECT email FROM paymybuddy_test.user_app WHERE user_app.email = :email")
 	public UserApp findByEmail(@Param("email")String email);*/
-	public UserApp findByEmail(String email);
+	public Optional<UserApp> findByEmail(String email);
 	
 	//public Iterable<UserApp >findByRoles(String name);
 }
--- src/main/java/com/paymybuddy/api/service/UserAccount.java
+++ src/main/java/com/paymybuddy/api/service/UserAccount.java
@@ -1,12 +1,12 @@
 package com.paymybuddy.api.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.paymybuddy.api.domain.DTO.UserContactDTO;
 import com.paymybuddy.api.domain.DTO.UserDTO;
 import com.paymybuddy.api.domain.DTO.UserLoginDTO;
 import com.paymybuddy.api.domain.DTO.UserMapper;
@@ -28,7 +28,12 @@ public class UserAccount {
 	@Autowired
 	private UserMapper mapper;
 
-	public UserApp createUser(UserApp userApp) {
+	public UserApp createUser(String email) {
+		UserApp userFound = userRepository.findByEmail(email).get();
+		return userFound;
+	}
+	
+	public UserApp getUser(UserApp userApp) {
 		String userPassword =userApp.getPassword();
 		String userPasswordEncoded=passwordEncoder.encode(userPassword);
 		userApp.setPassword(userPasswordEncoded);
@@ -36,23 +41,23 @@ public class UserAccount {
 	}
 
 	public UserLoginDTO getUserLoginByEmail(String email) {
-		UserApp user = userRepository.findByEmail(email);
+		UserApp user = userRepository.findByEmail(email).get();
 		UserLoginDTO userLoginDTO = mapper.UserToUserLoginDTO(user);
 		System.out.println(userLoginDTO );
 		return userLoginDTO;
 	}
 	
 	public UserDTO getUserByEmail(String email) {
-		UserApp user = userRepository.findByEmail(email);
+		UserApp user = userRepository.findByEmail(email).get();
 		UserDTO userDTO = mapper.UserToUserDTO(user);
 		System.out.println(userDTO );
 		return userDTO;
 	}
 	
 	public void addUserContact(String  email,String  emailContact) {
-		UserApp userContact = userRepository.findByEmail(emailContact);
+		UserApp userContact = userRepository.findByEmail(emailContact).get();
 		//UserContactDTO userContactDTO = mapper.UserToUserContactDTO(userContact);
-		UserApp user = userRepository.findByEmail(email);
+		UserApp user = userRepository.findByEmail(email).get();
 		List<UserApp> contactsOfUser=user.getContacts();
 		contactsOfUser.add(userContact );
 		user.setContacts(contactsOfUser);
