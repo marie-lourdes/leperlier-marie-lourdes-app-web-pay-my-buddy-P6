39151126a2cb4190058b203c16ab7341e60e7d58
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-01-09 07:53:49 +0100

    create template contacts

--- src/main/java/com/paymybuddy/api/controller/UserAccountController.java
+++ src/main/java/com/paymybuddy/api/controller/UserAccountController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import com.paymybuddy.api.domain.DTO.ContactDTO;
 import com.paymybuddy.api.service.UserAccount;
@@ -28,11 +30,16 @@ public class UserAccountController {
 		return "home";
 	}
 	
-	@GetMapping("/account/{id}/contact")// enpoint ressource contact , pas endpoint template!
+/*	@GetMapping("/account/{id}/contact")// enpoint ressource contact , pas endpoint template!
 	@ResponseBody
 	public ResponseEntity<List<ContactDTO>> getUserContact(@PathVariable  String id ) {
 		List<ContactDTO> allContact = userAccount.findUserContacts(id); 
 		return  ResponseEntity.status(HttpStatus.OK).body(allContact );
+	}*/
+	@GetMapping("/account/{id}/contact")// enpoint template contacts
+	public String getUserContact(Model model,@PathVariable  String id ) {
+		List<ContactDTO> allContact = userAccount.findUserContacts(id);
+		model.addAttribute("contacts", allContact );
+		return "contacts";
 	}
-
 }
