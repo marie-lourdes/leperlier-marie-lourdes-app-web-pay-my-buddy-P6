9146b3154c036c9f1855a0e147f357e5d36ce7bc
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-01-09 08:46:46 +0100

    add method post with route '/login' in LoginController

--- src/main/java/com/paymybuddy/api/controller/LoginController.java
+++ src/main/java/com/paymybuddy/api/controller/LoginController.java
@@ -1,15 +1,31 @@
 package com.paymybuddy.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.paymybuddy.api.domain.DTO.UserLoginDTO;
+import com.paymybuddy.api.domain.model.UserApp;
+import com.paymybuddy.api.service.UserAccount;
 
 @Controller
 public class LoginController {
+	@Autowired
+	private UserAccount userAccount ; ;
 	
 	@GetMapping("/login") //
 	public String getLoginPage() {
 		return "login";
 	}
+	
+	/*@PostMapping("/login") //
+	public UserLoginDTO saveLogin(@ModelAttribute UserLoginDTO user){
+		UserLoginDTO current =userAccount.getUserLoginByEmail(user.getEmail());
+		return current;
+	}*/
 	
 	@GetMapping("/logout") //
 	public String getLogoutPage() {
--- src/main/java/com/paymybuddy/api/controller/UserAccountController.java
+++ src/main/java/com/paymybuddy/api/controller/UserAccountController.java
@@ -3,23 +3,20 @@ package com.paymybuddy.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 
 import com.paymybuddy.api.domain.DTO.ContactDTO;
+import com.paymybuddy.api.service.AuthenticationUserDetailService;
 import com.paymybuddy.api.service.UserAccount;
 
 @Controller
 public class UserAccountController {
 	@Autowired
 	private UserAccount userAccount ;
-	
+
 	@GetMapping("/sign-up")
 	public String getSignUpPage() {
 		return "sign-up";
@@ -36,6 +33,7 @@ public class UserAccountController {
 		List<ContactDTO> allContact = userAccount.findUserContacts(id); 
 		return  ResponseEntity.status(HttpStatus.OK).body(allContact );
 	}*/
+	
 	@GetMapping("/account/{id}/contact")// enpoint template contacts
 	public String getUserContact(Model model,@PathVariable  String id ) {
 		List<ContactDTO> allContact = userAccount.findUserContacts(id);
