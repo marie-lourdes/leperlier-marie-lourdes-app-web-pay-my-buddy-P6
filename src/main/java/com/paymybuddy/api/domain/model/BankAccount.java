6ff5534de1aa82108fb78dd0bd9fba3f5ba6e52d
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-01-11 18:34:31 +0100

    create class BankAccount extends class abstract Account

--- src/main/java/com/paymybuddy/api/domain/model/BankAccount.java
+++ src/main/java/com/paymybuddy/api/domain/model/BankAccount.java
@@ -1,5 +1,11 @@
 package com.paymybuddy.api.domain.model;
 
-public class BankAccount {
-
+public class BankAccount extends Account{
+	public BankAccount(String id,String userId,double solde){
+		 super();
+		 super.id=id;
+		 super.userId=userId;
+		 super.solde=solde;
+		 super.type="buddy account";
+	 }
 }
--- src/main/java/com/paymybuddy/api/domain/model/BuddyAccount.java
+++ src/main/java/com/paymybuddy/api/domain/model/BuddyAccount.java
@@ -1,7 +1,7 @@
 package com.paymybuddy.api.domain.model;
 
 public class BuddyAccount extends Account {
-	//private  String type;
+	
 	public BuddyAccount(String id,String userId,double solde){
 		 super();
 		 super.id=id;
