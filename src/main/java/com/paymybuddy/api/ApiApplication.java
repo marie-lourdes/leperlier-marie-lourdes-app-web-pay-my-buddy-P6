97ff3df32b86e9560edddda2966fe8c662f8d5e8
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-01-16 14:48:19 +0100

    ApiApplication.java: delete test feature of  UserAppService methods in method run() of CommandLineRunner

--- src/main/java/com/paymybuddy/api/ApiApplication.java
+++ src/main/java/com/paymybuddy/api/ApiApplication.java
@@ -21,34 +21,6 @@ public class ApiApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) {
 
-		// userAccount.addUserAccount("testuser2@gmail.com");
-		/*
-		 * Contact userContact = new Contact();
-		 * userContact.setIdContact("testuser1@gmail.com");s
-		 * userContact.setFirstName("firstname1"); userContact.setLastName("lastname1");
-		 */
-
-		// userAccount.addUserContact("testuser1@gmail.com","testuser2@gmail.com");
-		// userAccount.addUserContact("testuser1@gmail.com","testuser3@gmail.com");
-		// userAccount.addUserContact("testuser3@gmail.com","testuser2@gmail.com");
-		// userAccount.findUserContacts("testuser2@gmail.com");
-		// userAccount.addUserContact("testuser3@gmail.com","testuser2@gmail.com");
-		// UserApp user= userAccount.getUserEntityByEmail("testuser3@gmail.com");
-		// userAccount.findUserContacts("testuser2@gmail.com");
-		/*
-		 * UserApp userTest2=userAccount.getUserEntityByEmail("testuser2@gmail.com");
-		 * Account bankingAccountUser2 = new Account();
-		 * bankingAccountUser2.setId(76001); bankingAccountUser2.setUser(userTest2);
-		 * bankingAccountUser2.setBalance(64569);
-		 * bankingAccountUser2.setType("Banking Account");
-		 */
-
-		// BankAccount bankAccount = new
-		// BankAccount(100824,"testuser2@gmail.com",787378.0124);
-		// System.out.println(accountApp);
-		// accountRepository.save(accountApp);
-		// accountRepository.save( bankingAccountUser2);
-		// accountRepository.findByUser("testuser2@gmail.com");
 
 	}
 }
