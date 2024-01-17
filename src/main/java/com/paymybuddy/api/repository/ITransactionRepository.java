4faf836c37f508d9d9423bbb68fbe6390d66cbcc
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-01-17 02:31:46 +0100

    ITransactionRepository.java: add method Transaction findByCreditUser(String userId)

--- src/main/java/com/paymybuddy/api/repository/ITransactionRepository.java
+++ src/main/java/com/paymybuddy/api/repository/ITransactionRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.paymybuddy.api.domain.model.Transaction;
 
-public interface ITransactionRepository extends JpaRepository<Transaction, Long>{
-
+public interface ITransactionRepository extends JpaRepository<Transaction, Long> {
+	public Transaction findByCreditUser(String userId);
 }
