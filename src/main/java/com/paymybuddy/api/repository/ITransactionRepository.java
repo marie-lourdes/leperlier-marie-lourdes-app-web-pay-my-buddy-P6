0b4ec63c2f79fe0f14c40c700c3147dafb94c2e4
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-01-18 01:01:49 +0100

    ITransactionRepository.java: clean code

--- src/main/java/com/paymybuddy/api/repository/ITransactionRepository.java
+++ src/main/java/com/paymybuddy/api/repository/ITransactionRepository.java
@@ -10,17 +10,5 @@ import com.paymybuddy.api.domain.model.UserApp;
 public interface ITransactionRepository extends JpaRepository<Transaction, Long> {
 	public List<Transaction> findByCreditUser(UserApp userId);
 
-	/* @Modifying
-	 @Query(value=" insert into  Transaction  (date,creditAccount,creditUser ,beneficiaryAccount,beneficiaryUser,description, amount, transactionFees) "
-	 		+ "values(:date,:credit_account_id,:credit_user_id ,:beneficiary_count_id,:beneficiary_user_id,:description,:amount,:transaction_fees)")
-	public void saveTransaction(
-			@Param("date ")Date date,
-			@Param("credit_user_id ")UserApp creditUser,
-			@Param("credit_account_id")Account creditAccount,
-			@Param("beneficiary_count_id ")Account beneficiaryAccount,
-			@Param("beneficiary_user_id")UserApp beneficiaryUser,
-			@Param("description")String description,
-			@Param("amount")double amount,
-			@Param("transaction_fees")float transactionFees);*/
 }
 
