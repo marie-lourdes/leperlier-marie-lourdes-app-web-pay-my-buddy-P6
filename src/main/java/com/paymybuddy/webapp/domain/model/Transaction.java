51b90eb75683c86595a66d8e75d7064e18033d75
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-01-19 02:56:33 +0100

    Transaction.java:add constraint jakarta @Positive on attribut amount of Transaction model

--- src/main/java/com/paymybuddy/webapp/domain/model/Transaction.java
+++ src/main/java/com/paymybuddy/webapp/domain/model/Transaction.java
@@ -1,4 +1,4 @@
-package  com.paymybuddy.webapp.domain.model;
+package com.paymybuddy.webapp.domain.model;
 
 import java.util.Date;
 
@@ -16,7 +16,7 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name="transaction")
+@Table(name = "transaction")
 public class Transaction {
 
 	@Id
@@ -27,39 +27,40 @@ public class Transaction {
 	@NotNull
 	@Column(name = "transaction_date_time ")
 	private Date date;
-	
+
 	@NotNull
 	@ManyToOne
-	@JoinColumn(name = "credit_user_id", nullable=false)
+	@JoinColumn(name = "credit_user_id", nullable = false)
 	private UserApp creditUser;
-	
+
 	@NotNull
 	@ManyToOne
-	@JoinColumn(name = "beneficiary_user_id", nullable=false)
+	@JoinColumn(name = "beneficiary_user_id", nullable = false)
 	private UserApp beneficiaryUser;
-	
+
 	@NotNull
 	@Column(name = "description")
 	private String description;
-	
+
 	@Positive
 	@NotNull
 	@Column(name = "amount")
 	private double amount;
-		
+
 	@NotNull
 	@Column(name = "transaction_fees")
 	private float transactionFees;
-	
-	public  Transaction() {}
-	
-	public  Transaction(Date date,UserApp creditUser,UserApp beneficiaryUser,String description,double amount, float transactionFees) {
-		this.date=date;
-		this.creditUser= creditUser;
-		this.beneficiaryUser=beneficiaryUser;
-		this.description=description;
-		this.amount=amount;
-		this.transactionFees=transactionFees;
+
+	public Transaction() {
 	}
-	
+
+	public Transaction(Date date, UserApp creditUser, UserApp beneficiaryUser, String description, double amount,
+			float transactionFees) {
+		this.date = date;
+		this.creditUser = creditUser;
+		this.beneficiaryUser = beneficiaryUser;
+		this.description = description;
+		this.amount = amount;
+		this.transactionFees = transactionFees;
+	}
 }
