ee7d2b52c2e5396d897234b8488a2217ed979d6d
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-01-20 20:11:20 +0100

    Transaction.java: refactor attribut accoutnBeneficiaryUser

--- src/main/java/com/paymybuddy/webapp/domain/model/Transaction.java
+++ src/main/java/com/paymybuddy/webapp/domain/model/Transaction.java
@@ -2,7 +2,6 @@ package com.paymybuddy.webapp.domain.model;
 
 import java.util.Date;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
