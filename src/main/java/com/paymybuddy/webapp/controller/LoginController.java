403ee6cbf4e7cc490a3d911ab2d0f41efeeffbb5
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-01-30 17:02:48 +0100

    LoginController.java: add method route GET ('/logout') with call method isUserOrAdmin(model, principal, logout)

--- src/main/java/com/paymybuddy/webapp/controller/LoginController.java
+++ src/main/java/com/paymybuddy/webapp/controller/LoginController.java
@@ -1,7 +1,6 @@
 package com.paymybuddy.webapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.web.exchanges.HttpExchange.Principal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
