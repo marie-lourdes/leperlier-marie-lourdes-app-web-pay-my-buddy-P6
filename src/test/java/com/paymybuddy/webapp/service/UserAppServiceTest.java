89ac695ca622ea5bc1c90c86eac7eb2c7c817d84
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-02-02 19:26:40 +0100

    create UserAppServiceTest

---
+++ src/test/java/com/paymybuddy/webapp/service/UserAppServiceTest.java
@@ -0,0 +1,16 @@
+package com.paymybuddy.webapp.service;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAppServiceTest {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+	}
+
+}
