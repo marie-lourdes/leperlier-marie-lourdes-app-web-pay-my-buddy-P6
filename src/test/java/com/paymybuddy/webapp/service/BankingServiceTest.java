c353640db8efd19d50c54a2f4122efba52ee2099
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-02-03 18:07:38 +0100

    creae BankingServiceTest

---
+++ src/test/java/com/paymybuddy/webapp/service/BankingServiceTest.java
@@ -0,0 +1,14 @@
+package com.paymybuddy.webapp.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BankingServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
