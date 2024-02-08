d62d4dd6cd1748ee805bae7add329a0e8cb711cd
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-02-08 07:46:06 +0100

    merge testing refactoring branch

--- src/main/java/com/paymybuddy/webapp/WebappApplication.java
+++ src/main/java/com/paymybuddy/webapp/WebappApplication.java
@@ -1,15 +1,7 @@
 package com.paymybuddy.webapp;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.paymybuddy.webapp.repository.IUserRepository;
-import com.paymybuddy.webapp.service.PaymentService;
-import com.paymybuddy.webapp.service.TransactionService;
-import com.paymybuddy.webapp.service.UserAccountService;
 
 @SpringBootApplication
 public class WebappApplication  {
